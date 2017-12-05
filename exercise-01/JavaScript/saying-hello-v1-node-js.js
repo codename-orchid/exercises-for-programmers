24048df07ba90dbd3193e87b1726eaa5735e6622
Cattleya Tantri <codename.orchid@gmail.com>
2017-12-05 12:36:13 +1100

    Renamed folders. Completed exercise 01 version 1 in JavaScript (vanilla JS and node.js).

---
+++ exercise-01/JavaScript/saying-hello-v1-node-js.js
@@ -0,0 +1,19 @@
+/*
+ * Exercises for programmers, exercise 01: Saying hello
+ * Version 1
+ * codename-orchid
+ */
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What is your name? ', (name) => {
+  const hello = 'Hello, ';
+  const nice = ', nice to meet you!';
+  console.log(hello + name + nice);
+  rl.close();
+})
