afa5a5120b642fe02f44402a8171ee534606f412
Cattleya Tantri <codename.orchid@gmail.com>
2017-12-06 12:25:04 +1100

    Completed exercise 01 version 2 in JavaScript (vanilla JS and Node.js).

--- exercise-01/JavaScript/saying-hello-v2-node-js.js
+++ exercise-01/JavaScript/saying-hello-v2-node-js.js
@@ -1,8 +1,8 @@
 /*
  * Exercises for programmers, exercise 01: Saying hello
- * Version 1
+ * Version 2
  * codename-orchid
- * Reference: https://nodejs.org/api/readline.html
+ * reference: https://nodejs.org/api/readline.html
  */
 
 const readline = require('readline');
@@ -12,9 +12,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What is your name? ', (name) => {
-  const hello = 'Hello, ';
-  const nice = ', nice to meet you!';
-  console.log(hello + name + nice);
+rl.question('What is your name? ', name => {
+  console.log('Hello, ' + name + ', nice to meet you!');
   rl.close();
 })
